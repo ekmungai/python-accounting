9783ce071e25119934d1cbad11d9d4924a689432
Edward Mungai <emungai@pinecrest.co.ke>
2024-02-10 16:59:36 +0100

    Update: Complete config api reference

--- src/config.py
+++ src/config.py
@@ -6,11 +6,11 @@
 # the MIT License: https://www.opensource.org/licenses/mit-license.php
 
 """
-This module provides the configuration for python accounting. Its properties are populated from the
-config.toml by default and should be adequate for most settings (except maybe the datatabse 
-configuration), but being a plain python object any of the attributes may be overriden by
-assingment at any point. For more extensive custom configurations, you can initialize the
-class with a custom config.tml file.
+This module provides the configuration for python accounting. Its properties are populated from 
+`config.toml <https://github.com/ekmungai/python-accounting/blob/main/config.toml>`__ by default and 
+should be adequate for most settings (except maybe the database configuration), but being a plain
+python object any of the attributes may be overriden by assingment at any point. For more extensive
+custom configurations, you can initialize the class with a custom config.tml file.
 
 """
 import toml
@@ -26,10 +26,13 @@ class Config:
     The database configuration.
     ::
         {
-            url (str): The database connection string.
-            echo (bool): Whether to output the SqlAlchemy generated queries to the console. 
-            include_deleted (bool): Whether to include soft deleted records in query results. 
-            echo (bool): Whether to include records from all entities in query results. 
+            url (str): The database connection string. Defaults to 'sqlite://'.
+            echo (bool): Whether to output the SqlAlchemy generated queries to the console.
+            Defaults to false.
+            include_deleted (bool): Whether to include soft deleted records in query results.
+            Defaults to false. 
+            echo (bool): Whether to include records from all entities in query results.
+            Defaults to false.
         }
     """
     hashing: dict
@@ -37,14 +40,20 @@ class Config:
     Configuration for hashing Ledger records to guard against direct database tampering.
     ::
         {
-            salt (str): The initial salt for .
-            echo (bool): Whether to output the SqlAlchemy generated queries to the console. 
-            include_deleted (bool): Whether to include soft deleted records in query results. 
-            echo (bool): Whether to include records from all entities in query results. 
+            salt (str): The initial value for the ledger hashing. Defaults to 'hashing salt'.
+            algorithm (str): The Algorithm to use for encoding the hashes. Defaults to 'sha256'.
+        }
+    """
+    dates: dict
+    """
+    Configuration for formatting dates in reports.
+    ::
+        {
+            short (str): The format for short dates. Defaults to '%Y-%m-%d'.
+            long (str): The format for long dates. Defaults to '%d, %b %Y'.
         }
     """
     accounts: dict
-    dates: dict
     """
     Account Types, Codes and Financial Report section membership Configuration
     ::
@@ -52,7 +61,7 @@ class Config:
             types (dict): The Account Types as defined by GAAP/IFRS:
             label (str): The human readable name of the Account Type.
             account_code (int): The starting point for the genaration of Account Codes for
-             Accounts of the type.
+            Accounts of the type.
             purchasables (list): Account types that can be used in purchasing Transations. 
         }
     """
@@ -63,14 +72,25 @@ class Config:
         {
             types (`dict`): The Transaction Types as defined by GAAP/IFRS
             label (`str`): The human readable name of the Transaction Type.
-            transaction_no_prefix (`int`): The code to be inserted at the beginning
-             of the Transaction's type's sequentially generated transaction number.
+            transaction_no_prefix (`int`): The code to be inserted at the beginningof the
+            Transaction's type's sequentially generated transaction number.
             clearables (`list`): Transaction types that can cleared by assignable Transactions.  
             assignables (`list`): Transaction types that can user to cleared (have assigned
-             to them) clearable Transactions.
+            to them) clearable Transactions.
         }  
     """
     reports: dict
+    """
+    Configuration for Financial Reports.
+    ::
+        {
+            indent_length (`int`): Number of spaces to indent report sections. Defaults to 4.
+            result_length (`int`): Number of characters to underline report results. Defaults
+            to 15.
+            aging_schedule_brackets (`dict`): The labels and max age in days for aging schedule
+            brackets.
+        }  
+    """
 
     def __init__(self, config_file="config.toml") -> None:
         with open(config_file, "r", -1, "UTF-8") as f:
