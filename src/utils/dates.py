7570a972a036b42875f876f82a29d2a379e84396
Edward Mungai <emungai@pinecrest.co.ke>
2024-02-20 19:52:31 +0100

    Update: Complete transactions and utils module api reference

--- src/utils/dates.py
+++ src/utils/dates.py
@@ -14,7 +14,13 @@ from src.models.reporting_period import ReportingPeriod
 
 
 def get_dates(session, start_date: datetime = None, end_date: datetime = None) -> tuple:
-    """Returns the start, end and period start dates given the inputs provided."""
+    """
+    Returns the start, end and period start dates given the inputs provided.
+
+    Args:
+        start_date (datetime): The start date.
+        end_date (datetime): The end date.
+    """
     end_date = datetime.today() if not end_date else end_date
     period = ReportingPeriod.get_period(
         session, datetime(end_date.year, session.entity.year_start, 1, 0, 0, 0)
